b9f54dc693a19840e05df6226278b4cb69693e12
Manuel Grimaldi <manuel.grimaldi@xpeppers.com>
2020-11-20 15:52:55 +0100

    can edit update the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,22 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def create
